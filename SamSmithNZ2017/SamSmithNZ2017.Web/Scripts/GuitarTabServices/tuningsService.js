0a75e1b5771b780e6aa84d283ef4878719517d1d
Sam Smith <samsmithnz@gmail.com>
2017-04-10 13:23:50 -0400

    edit tab now showing data correctly

---
+++ SamSmithNZ2017/SamSmithNZ2017.Web/Scripts/GuitarTabServices/tuningsService.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('GuitarTabApp')
+        .service('tuningsService', tuningsService);
+    tuningsService.$inject = ['$http'];//, '$q', 'configSettings'];
+
+    function tuningsService($http) {//, $q, configSettings) {
+        //Read config settings
+        //var baseUrl = configSettings.webApiBaseUrl;
+        //var baseUrl = 'http://localhost:12730/';
+        var baseUrl = 'http://ssnzguitartabservice.azurewebsites.net/';
+
+        this.getTunings = function () {
+            var url = baseUrl + 'api/Tuning/GetTunings';
+            console.log(url);
+            return $http.get(url);
+        };
+
+    }
+})();
