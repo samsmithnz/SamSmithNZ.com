2feb84ea565ea603af70137b8581e4ff75770286
Sam Smith <samsmithnz@gmail.com>
2017-05-09 16:58:04 -0400

    Added dotnet code demos for steam API

---
+++ SamSmithNZ2017/SSNZ.Steam.CoreService/Controllers/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SSNZ.Steam.CoreData;
+using SSNZ.Steam.CoreModels;
+
+namespace SSNZ.Steam.CoreService.Controllers
+{
+    [Route("api/[controller]")]
+    public class PlayerController : Controller
+    {
+        // GET api/values/5
+        [HttpGet("{steamID}")]
+        public async Task<Player> GetPlayer(string steamID)
+        {
+            PlayerDA da = new PlayerDA();
+            return await da.GetDataAsync(steamID);
+        }
+    }
+}
---
+++ SamSmithNZ2017/SSNZ.Steam.CoreService/Controllers/PlayerGamesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SSNZ.Steam.CoreData;
+using SSNZ.Steam.CoreModels;
+
+namespace SSNZ.Steam.CoreService.Controllers
+{
+    public class PlayerGamesController : Controller
+    {
+        public async Task<List<Game>> GetPlayer(string steamID)
+        {
+            PlayerGamesDA da = new PlayerGamesDA();
+            return await da.GetDataAsync(steamID);
+        }
+    }
+}
