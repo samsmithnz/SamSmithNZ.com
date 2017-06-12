9cb97ef99a5fb548ab7e1228824fa747135b424b
Sam Smith <samsmithnz@gmail.com>
2017-06-11 21:31:55 -0400

    Created basic playoff page, but there are no scores yet

--- SamSmithNZ2017/SamSmithNZ2017.Web/Scripts/IntFootballControllers/playOffsController.js
+++ SamSmithNZ2017/SamSmithNZ2017.Web/Scripts/IntFootballControllers/playOffsController.js
@@ -6,6 +6,7 @@
     playOffsController.$inject = ['$scope', '$http', 'gameService'];
     function playOffsController($scope, $http, gameService) {
         $scope.games = [];
+        $scope.showDebugInformation = true;
         var onError = function (data) {
             //errorHandlerService.errorHandler(data);
             console.log("Error!!");
@@ -14,10 +15,62 @@
         var onGetGamesEventComplete = function (response) {
             $scope.games = response.data;
             //console.log($scope.games);
+            $scope.show16s = true;
+            $scope.showQuarters = true;
+            $scope.showSemis = true;
+            $scope.show3rdPlace = true;
+            $scope.showFinals = true;
+            console.log('games: ' + $scope.games.length);
+            switch ($scope.games.length) {
+                case 16:
+                    //Show Everything!!!
+                    break;
+                case 8:
+                    //Only show Quarter Finals
+                    $scope.show16s = false;
+                    break;
+                case 7:
+                    //Show Quarter Finals, hide 3rd place
+                    $scope.show16s = false;
+                    $scope.show3rdPlace = false;
+                    break;
+                case 4:
+                    //Only show Semis
+                    $scope.show16s = false;
+                    $scope.showQuarters = false;
+                    break;
+                case 3:
+                    //Show Semis and Finals, hide 3rd Place
+                    $scope.show16s = false;
+                    $scope.showQuarters = false;
+                    $scope.show3rdPlace = false;
+                    break;
+                case 2:
+                    //Show Finals & 3rd place
+                    $scope.show16s = false;
+                    $scope.showQuarters = false;
+                    $scope.showSemis = false;
+                    break;
+                case 1:
+                    //Only show Finals
+                    $scope.show16s = false;
+                    $scope.showQuarters = false;
+                    $scope.showSemis = false;
+                    $scope.show3rdPlace = false;
+                    break;
+            }
         };
-        console.log("TeamCode: " + getUrlParameter('TeamCode'));
-        $scope.teamCode = getUrlParameter('TeamCode');
-        gameService.getGamesForPlayoffs($scope.teamCode).then(onGetGamesEventComplete, onError);
+        $scope.tournamentCode = getUrlParameter('TournamentCode');
+        $scope.roundNumber = getUrlParameter('RoundNumber');
+        gameService.getGamesForPlayoffs($scope.tournamentCode, $scope.roundNumber).then(onGetGamesEventComplete, onError);
+        $scope.findGame = function (gameNumber) {
+            for (var i = 0; i <= $scope.games.length - 1; i++) {
+                if ($scope.games[i].GameNumber == gameNumber) {
+                    return $scope.games[i];
+                }
+            }
+            return null;
+        };
     }
     function getUrlParameter(param) {
         var sPageURL = (window.location.search.substring(1));
--- SamSmithNZ2017/SamSmithNZ2017.Web/Scripts/IntFootballControllers/playOffsController.ts
+++ SamSmithNZ2017/SamSmithNZ2017.Web/Scripts/IntFootballControllers/playOffsController.ts
@@ -9,6 +9,7 @@
     function playOffsController($scope, $http, gameService) {
         
         $scope.games = [];
+        $scope.showDebugInformation = true;
 
         var onError = function (data) {
             //errorHandlerService.errorHandler(data);
@@ -19,13 +20,69 @@
          var onGetGamesEventComplete = function (response) {
             $scope.games = response.data;
             //console.log($scope.games);
+
+            $scope.show16s = true;
+            $scope.showQuarters = true;
+            $scope.showSemis = true;
+            $scope.show3rdPlace = true;
+            $scope.showFinals = true;
+
+            console.log('games: ' + $scope.games.length);
+
+            switch ($scope.games.length) {
+                case 16:
+                    //Show Everything!!!
+                    break;
+                case 8:
+                    //Only show Quarter Finals
+                    $scope.show16s = false;
+                    break;
+                case 7:
+                    //Show Quarter Finals, hide 3rd place
+                    $scope.show16s = false;
+                    $scope.show3rdPlace = false;
+                    break;
+                case 4:
+                    //Only show Semis
+                    $scope.show16s = false;
+                    $scope.showQuarters = false;
+                    break;
+                case 3:
+                    //Show Semis and Finals, hide 3rd Place
+                    $scope.show16s = false;
+                    $scope.showQuarters = false;
+                    $scope.show3rdPlace = false;
+                    break;
+                case 2:
+                    //Show Finals & 3rd place
+                    $scope.show16s = false;
+                    $scope.showQuarters = false;
+                    $scope.showSemis = false;
+                    break;
+                case 1:
+                    //Only show Finals
+                    $scope.show16s = false;
+                    $scope.showQuarters = false;
+                    $scope.showSemis = false;
+                    $scope.show3rdPlace = false;
+                    break;
+            }
         }
 
-        console.log("TeamCode: " + getUrlParameter('TeamCode'));
-        $scope.teamCode = getUrlParameter('TeamCode');
+         $scope.tournamentCode = getUrlParameter('TournamentCode');
+         $scope.roundNumber = getUrlParameter('RoundNumber');
 
-        gameService.getGamesForPlayoffs($scope.teamCode).then(onGetGamesEventComplete, onError);
+         gameService.getGamesForPlayoffs($scope.tournamentCode, $scope.roundNumber).then(onGetGamesEventComplete, onError);
 
+         $scope.findGame = function (gameNumber){
+             for (var i = 0; i <= $scope.games.length - 1; i++) {
+                 if ($scope.games[i].GameNumber == gameNumber)
+                 {
+                     return $scope.games[i];
+                 }
+             }
+             return null;
+         }
     }
 
     function getUrlParameter(param: string) {
