c214f3379565e28da4d776956992def51b8b4e27
Sam Smith <samsmithnz@gmail.com>
2017-06-30 08:27:16 -0400

    Update to text of buttons

--- SamSmithNZ2017/SSNZ.ITunes.ImporterWin/frmMain.Designer.cs
+++ SamSmithNZ2017/SSNZ.ITunes.ImporterWin/frmMain.Designer.cs
@@ -39,9 +39,11 @@
             // 
             // btnImport
             // 
-            this.btnImport.Location = new System.Drawing.Point(12, 40);
+            this.btnImport.Enabled = false;
+            this.btnImport.Location = new System.Drawing.Point(18, 62);
+            this.btnImport.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnImport.Name = "btnImport";
-            this.btnImport.Size = new System.Drawing.Size(95, 23);
+            this.btnImport.Size = new System.Drawing.Size(142, 35);
             this.btnImport.TabIndex = 84;
             this.btnImport.Text = "Import Playlist";
             this.btnImport.UseVisualStyleBackColor = true;
@@ -49,17 +51,19 @@
             // 
             // txtFile
             // 
-            this.txtFile.Location = new System.Drawing.Point(12, 14);
+            this.txtFile.Location = new System.Drawing.Point(18, 22);
+            this.txtFile.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtFile.Name = "txtFile";
-            this.txtFile.Size = new System.Drawing.Size(496, 20);
+            this.txtFile.Size = new System.Drawing.Size(742, 26);
             this.txtFile.TabIndex = 85;
             this.txtFile.Text = "C:\\Projects\\SSNZDefault\\SamSmithNZ\\SamSmithNZ\\ITunesSourceFiles\\";
             // 
             // btnOpenFile
             // 
-            this.btnOpenFile.Location = new System.Drawing.Point(514, 12);
+            this.btnOpenFile.Location = new System.Drawing.Point(771, 18);
+            this.btnOpenFile.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnOpenFile.Name = "btnOpenFile";
-            this.btnOpenFile.Size = new System.Drawing.Size(30, 23);
+            this.btnOpenFile.Size = new System.Drawing.Size(45, 35);
             this.btnOpenFile.TabIndex = 86;
             this.btnOpenFile.Text = "...";
             this.btnOpenFile.UseVisualStyleBackColor = true;
@@ -72,40 +76,44 @@
             // lblStatus
             // 
             this.lblStatus.AutoSize = true;
-            this.lblStatus.Location = new System.Drawing.Point(12, 66);
+            this.lblStatus.Location = new System.Drawing.Point(18, 102);
+            this.lblStatus.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblStatus.Name = "lblStatus";
-            this.lblStatus.Size = new System.Drawing.Size(35, 13);
+            this.lblStatus.Size = new System.Drawing.Size(51, 20);
             this.lblStatus.TabIndex = 88;
             this.lblStatus.Text = "label1";
             // 
             // prgStatus
             // 
-            this.prgStatus.Location = new System.Drawing.Point(12, 82);
+            this.prgStatus.Location = new System.Drawing.Point(18, 126);
+            this.prgStatus.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.prgStatus.Name = "prgStatus";
-            this.prgStatus.Size = new System.Drawing.Size(532, 23);
+            this.prgStatus.Size = new System.Drawing.Size(798, 35);
             this.prgStatus.TabIndex = 89;
             // 
             // btnFillInPlayListGaps
             // 
-            this.btnFillInPlayListGaps.Location = new System.Drawing.Point(407, 41);
+            this.btnFillInPlayListGaps.Location = new System.Drawing.Point(549, 63);
+            this.btnFillInPlayListGaps.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnFillInPlayListGaps.Name = "btnFillInPlayListGaps";
-            this.btnFillInPlayListGaps.Size = new System.Drawing.Size(137, 23);
+            this.btnFillInPlayListGaps.Size = new System.Drawing.Size(267, 35);
             this.btnFillInPlayListGaps.TabIndex = 90;
-            this.btnFillInPlayListGaps.Text = "Fill in playlist gaps";
+            this.btnFillInPlayListGaps.Text = "Fill in playlist gaps (USE ME)";
             this.btnFillInPlayListGaps.UseVisualStyleBackColor = true;
             this.btnFillInPlayListGaps.Click += new System.EventHandler(this.btnFillInPlayListGaps_Click);
             // 
             // frmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(556, 117);
+            this.ClientSize = new System.Drawing.Size(834, 180);
             this.Controls.Add(this.btnFillInPlayListGaps);
             this.Controls.Add(this.prgStatus);
             this.Controls.Add(this.lblStatus);
             this.Controls.Add(this.btnImport);
             this.Controls.Add(this.btnOpenFile);
             this.Controls.Add(this.txtFile);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "frmMain";
             this.Text = "Form1";
             this.ResumeLayout(false);
