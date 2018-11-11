eb88006aea19951f30753d3c21c63ecca0203c23
Sam Smith <samsmithnz@gmail.com>
2018-11-11 21:03:40 +0000

    Merged PR 170: Added new selenium smoke tests for every environment
    Added new selenium smoke tests for every environment

---
+++ SamSmithNZ2017/SamSmithNZ2018.UITests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SamSmithNZ2018.UITests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SamSmithNZ2018.UITests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5fcebdb8-2911-4aa9-8eef-74a0c84eb59f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
