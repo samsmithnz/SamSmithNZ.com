51124006541e4b2a744e67a620c90c83ebb540c3
Sam Smith <samsmithnz@gmail.com>
2018-11-12 10:57:27 +0000

    Merged PR 172: Add sandbox item to fix vsts bug
    Add sandbox item to fix vsts bug

--- SamSmithNZ2017/SamSmithNZ2018.UITests/SSNZWebTest.cs
+++ SamSmithNZ2017/SamSmithNZ2018.UITests/SSNZWebTest.cs
@@ -7,7 +7,9 @@ namespace SamSmithNZ2018.UITests
     [TestClass]
     public class SSNZWebTest
     {
-        public TestContext TestContext { get; set; }
+        private ChromeDriver _Driver;
+        private TestContext testContextInstance;
+
         private string _SSNZWebUrl = null;
         private string _SSNZSteamServiceUrl = null;
         private string _SSNZFFServiceUrl = null;
@@ -19,6 +21,11 @@ namespace SamSmithNZ2018.UITests
         [TestInitialize]
         public void SetupTests()
         {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.AddArguments("headless");
+            chromeOptions.AddArgument("no-sandbox");
+            _Driver = new ChromeDriver(chromeOptions);
+
             if (TestContext.Properties == null || TestContext.Properties.Count == 0)
             {
                 _SSNZWebUrl = "https://www.samsmithnz.com/";
@@ -44,63 +51,192 @@ namespace SamSmithNZ2018.UITests
         [TestMethod]
         [TestCategory("SkipWhenLiveUnitTesting")]
         [TestCategory("SmokeTest")]
-        public void GotoSSNZHomePage()
+        public void GotoSSNZHomePageTest()
         {
             //Arrange
-            ChromeOptions chromeOptions = new ChromeOptions();
-            chromeOptions.AddArguments("headless");
             bool ssnzWebLoaded = false;
-            bool ssnzSteamServiceLoaded = false;
-            bool ssnzFFServiceLoaded = false;
-            bool ssnzIntFootballServiceLoaded = false;
-            bool ssnzITunesServiceLoaded = false;
-            bool ssnzGuitarTabServiceLoaded = false;
-            bool ssnzMAndMServiceLoaded = false;
 
             //Act
-            using (ChromeDriver browser = new ChromeDriver(chromeOptions))
-            {
-
-                browser.Navigate().GoToUrl(this._SSNZWebUrl);
-                ssnzWebLoaded = (browser.Url == this._SSNZWebUrl);
-                //Assert.AreEqual("AppStack", Driver.FindElement(By.ClassName("navbar-brand")).Text);
-
-                string steamURL = this._SSNZSteamServiceUrl + "api/Player/GetPlayer?steamId=76561197971691578";
-                browser.Navigate().GoToUrl(steamURL);
-                ssnzSteamServiceLoaded = (browser.Url == steamURL);
-
-                string ffURL = this._SSNZFFServiceUrl + "";
-                browser.Navigate().GoToUrl(ffURL);
-                ssnzFFServiceLoaded = (browser.Url == ffURL);
-
-                string intFootballURL = this._SSNZIntFootballServiceUrl + "";
-                browser.Navigate().GoToUrl(intFootballURL);
-                ssnzIntFootballServiceLoaded = (browser.Url == intFootballURL);
-
-                string itunesURL = this._SSNZITunesServiceUrl + "";
-                browser.Navigate().GoToUrl(itunesURL);
-                ssnzITunesServiceLoaded = (browser.Url == itunesURL);
-
-                string guitartabURL = this._SSNZGuitarTabServiceUrl + "";
-                browser.Navigate().GoToUrl(guitartabURL);
-                ssnzGuitarTabServiceLoaded = (browser.Url == guitartabURL);
-
-                //string mandmURL = this._SSNZMAndMServiceUrl + "";
-                //browser.Navigate().GoToUrl(mandmURL);
-                //ssnzMAndMServiceLoaded = (browser.Url == mandmURL);
-
-            }
+            _Driver.Navigate().GoToUrl(this._SSNZWebUrl);
+            ssnzWebLoaded = (_Driver.Url == this._SSNZWebUrl);
+            //Assert.AreEqual("AppStack", Driver.FindElement(By.ClassName("navbar-brand")).Text);
 
             //Assert
             Assert.IsTrue(ssnzWebLoaded);
-            Assert.IsTrue(ssnzSteamServiceLoaded);
-            Assert.IsTrue(ssnzFFServiceLoaded);
-            Assert.IsTrue(ssnzIntFootballServiceLoaded);
-            Assert.IsTrue(ssnzITunesServiceLoaded);
-            Assert.IsTrue(ssnzGuitarTabServiceLoaded);
-            Assert.IsTrue(ssnzMAndMServiceLoaded == false);
-
         }
+
+        [TestMethod]
+        [TestCategory("SkipWhenLiveUnitTesting")]
+        [TestCategory("SmokeTest")]
+        public void GotoSSNZSteamServiceTest()
+        {
+            //Arrange
+            bool ssnzSteamServiceLoaded = false;
+
+            //Act
+            string steamURL = this._SSNZSteamServiceUrl + "api/Player/GetPlayer?steamId=76561197971691578";
+            _Driver.Navigate().GoToUrl(steamURL);
+            ssnzSteamServiceLoaded = (_Driver.Url == steamURL);
+
+            //Assert
+            Assert.IsTrue(ssnzSteamServiceLoaded);
+        }
+
+        [TestMethod]
+        [TestCategory("SkipWhenLiveUnitTesting")]
+        [TestCategory("SmokeTest")]
+        public void GotoSSNZFFServiceTest()
+        {
+            //Arrange
+            bool ssnzFFServiceLoaded = false;
+
+            //Act
+            string ffURL = this._SSNZFFServiceUrl + "";
+            _Driver.Navigate().GoToUrl(ffURL);
+            ssnzFFServiceLoaded = (_Driver.Url == ffURL);
+
+            //Assert
+            Assert.IsTrue(ssnzFFServiceLoaded);
+        }
+
+        [TestMethod]
+        [TestCategory("SkipWhenLiveUnitTesting")]
+        [TestCategory("SmokeTest")]
+        public void GotoSSNZIntFootballServiceTest()
+        {
+            //Arrange
+            bool ssnzIntFootballServiceLoaded = false;
+
+            //Act
+            string intFootballURL = this._SSNZIntFootballServiceUrl + "";
+            _Driver.Navigate().GoToUrl(intFootballURL);
+            ssnzIntFootballServiceLoaded = (_Driver.Url == intFootballURL);
+
+            //Assert
+            Assert.IsTrue(ssnzIntFootballServiceLoaded);
+        }
+
+        [TestMethod]
+        [TestCategory("SkipWhenLiveUnitTesting")]
+        [TestCategory("SmokeTest")]
+        public void GotoSSNZITunesServiceTest()
+        {
+            //Arrange
+            bool ssnzITunesServiceLoaded = false;
+
+            //Act
+            string itunesURL = this._SSNZITunesServiceUrl + "";
+            _Driver.Navigate().GoToUrl(itunesURL);
+            ssnzITunesServiceLoaded = (_Driver.Url == itunesURL);
+
+            //Assert
+            Assert.IsTrue(ssnzITunesServiceLoaded);
+        }
+
+        [TestMethod]
+        [TestCategory("SkipWhenLiveUnitTesting")]
+        [TestCategory("SmokeTest")]
+        public void GotoSSNZGuitarTabServiceTest()
+        {
+            //Arrange
+            bool ssnzGuitarTabServiceLoaded = false;
+
+            //Act
+            string guitartabURL = this._SSNZGuitarTabServiceUrl + "";
+            _Driver.Navigate().GoToUrl(guitartabURL);
+            ssnzGuitarTabServiceLoaded = (_Driver.Url == guitartabURL);
+
+            //Assert
+            Assert.IsTrue(ssnzGuitarTabServiceLoaded);
+        }
+
+        [TestMethod]
+        [TestCategory("SkipWhenLiveUnitTesting")]
+        [TestCategory("SmokeTest")]
+        public void GotoSSNZMandMServiceTest()
+        {
+            //Arrange
+            bool ssnzMAndMServiceLoaded = false;
+
+            //Act
+            //string mandmURL = this._SSNZMAndMServiceUrl + "";
+            //_Driver.Navigate().GoToUrl(mandmURL);
+            //ssnzMAndMServiceLoaded = (_Driver.Url == mandmURL);
+
+            //Assert
+            Assert.IsTrue(ssnzMAndMServiceLoaded == false);
+        }
+
+        [TestCleanup()]
+        public void CleanupTests()
+        {
+            _Driver.Quit();
+        }
+
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        //[TestMethod]
+        //[TestCategory("SkipWhenLiveUnitTesting")]
+        //[TestCategory("SmokeTest")]
+        //public void GotoSSNZHomePage()
+        //{
+        //    //Arrange
+        //    bool ssnzWebLoaded = false;
+        //    bool ssnzSteamServiceLoaded = false;
+        //    bool ssnzFFServiceLoaded = false;
+        //    bool ssnzIntFootballServiceLoaded = false;
+        //    bool ssnzITunesServiceLoaded = false;
+        //    bool ssnzGuitarTabServiceLoaded = false;
+        //    bool ssnzMAndMServiceLoaded = false;
+
+        //    //Act
+        //    _Driver.Navigate().GoToUrl(this._SSNZWebUrl);
+        //    ssnzWebLoaded = (_Driver.Url == this._SSNZWebUrl);
+        //    //Assert.AreEqual("AppStack", Driver.FindElement(By.ClassName("navbar-brand")).Text);
+
+        //    string steamURL = this._SSNZSteamServiceUrl + "api/Player/GetPlayer?steamId=76561197971691578";
+        //    _Driver.Navigate().GoToUrl(steamURL);
+        //    ssnzSteamServiceLoaded = (_Driver.Url == steamURL);
+
+        //    string ffURL = this._SSNZFFServiceUrl + "";
+        //    _Driver.Navigate().GoToUrl(ffURL);
+        //    ssnzFFServiceLoaded = (_Driver.Url == ffURL);
+
+        //    string intFootballURL = this._SSNZIntFootballServiceUrl + "";
+        //    _Driver.Navigate().GoToUrl(intFootballURL);
+        //    ssnzIntFootballServiceLoaded = (_Driver.Url == intFootballURL);
+
+        //    string itunesURL = this._SSNZITunesServiceUrl + "";
+        //    _Driver.Navigate().GoToUrl(itunesURL);
+        //    ssnzITunesServiceLoaded = (_Driver.Url == itunesURL);
+
+        //    string guitartabURL = this._SSNZGuitarTabServiceUrl + "";
+        //    _Driver.Navigate().GoToUrl(guitartabURL);
+        //    ssnzGuitarTabServiceLoaded = (_Driver.Url == guitartabURL);
+
+        //    //string mandmURL = this._SSNZMAndMServiceUrl + "";
+        //    //_Driver.Navigate().GoToUrl(mandmURL);
+        //    //ssnzMAndMServiceLoaded = (_Driver.Url == mandmURL);
+
+        //    //Assert
+        //    Assert.IsTrue(ssnzWebLoaded);
+        //    Assert.IsTrue(ssnzSteamServiceLoaded);
+        //    Assert.IsTrue(ssnzFFServiceLoaded);
+        //    Assert.IsTrue(ssnzIntFootballServiceLoaded);
+        //    Assert.IsTrue(ssnzITunesServiceLoaded);
+        //    Assert.IsTrue(ssnzGuitarTabServiceLoaded);
+        //    Assert.IsTrue(ssnzMAndMServiceLoaded == false);
+        //}
     }
 }
 
