68438f5e1bae1bef80f14a5c35231c9cad824e5d
Sam Smith (Microsoft) <samsmithnz@gmail.com>
2021-06-12 18:24:22 -0400

    Removed first round of warnings

---
+++ SamSmithNZ/SamSmithNZ.Web/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE0066:Convert switch statement to expression", Justification = "<Pending>", Scope = "member", Target = "~M:SamSmithNZ.Web.Models.GameDev.Setup.GetDirection(System.Int32)~System.String")]
+[assembly: SuppressMessage("Style", "IDE0066:Convert switch statement to expression", Justification = "<Pending>", Scope = "member", Target = "~M:SamSmithNZ.Web.Models.GameDev.Setup.GetOppositeDirection(System.String)~System.String")]
---
+++ SamSmithNZ/SamSmithNZ.WorldCupGoals.WPF/GlobalSuppressions.cs
@@ -0,0 +1,25 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:SamSmithNZ.WorldCupGoals.WPF.AssignGoals.btnDelete_Click(System.Object,System.Windows.RoutedEventArgs)")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:SamSmithNZ.WorldCupGoals.WPF.AssignGoals.btnSave_Click(System.Object,System.Windows.RoutedEventArgs)")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:SamSmithNZ.WorldCupGoals.WPF.AssignGoals.chkIsOwnGoal_Checked(System.Object,System.Windows.RoutedEventArgs)")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:SamSmithNZ.WorldCupGoals.WPF.AssignGoals.chkIsPenalty_Checked(System.Object,System.Windows.RoutedEventArgs)")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:SamSmithNZ.WorldCupGoals.WPF.AssignGoals.chkShowExtraTime_Checked(System.Object,System.Windows.RoutedEventArgs)")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:SamSmithNZ.WorldCupGoals.WPF.AssignGoals.chkShowExtraTime_Unchecked(System.Object,System.Windows.RoutedEventArgs)")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:SamSmithNZ.WorldCupGoals.WPF.AssignGoals.sliInjuryTime_ValueChanged(System.Object,System.Windows.RoutedPropertyChangedEventArgs{System.Double})")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:SamSmithNZ.WorldCupGoals.WPF.AssignGoals.sliNormalTime_ValueChanged(System.Object,System.Windows.RoutedPropertyChangedEventArgs{System.Double})")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:SamSmithNZ.WorldCupGoals.WPF.AssignGoals.txtInjuryTime_TextChanged(System.Object,System.Windows.Controls.TextChangedEventArgs)")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:SamSmithNZ.WorldCupGoals.WPF.AssignGoals.txtNormalTime_TextChanged(System.Object,System.Windows.Controls.TextChangedEventArgs)")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:SamSmithNZ.WorldCupGoals.WPF.AssignScore.btnSave_Click(System.Object,System.Windows.RoutedEventArgs)")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:SamSmithNZ.WorldCupGoals.WPF.AssignShootoutGoals.btnDelete_Click(System.Object,System.Windows.RoutedEventArgs)")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:SamSmithNZ.WorldCupGoals.WPF.AssignShootoutGoals.btnSave_Click(System.Object,System.Windows.RoutedEventArgs)")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:SamSmithNZ.WorldCupGoals.WPF.Goals.btnClose_Click(System.Object,System.Windows.RoutedEventArgs)")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:SamSmithNZ.WorldCupGoals.WPF.Goals.btnGoalEdit_Click(System.Object,System.Windows.RoutedEventArgs)")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:SamSmithNZ.WorldCupGoals.WPF.ShootoutGoals.btnClose_Click(System.Object,System.Windows.RoutedEventArgs)")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:SamSmithNZ.WorldCupGoals.WPF.ShootoutGoals.btnGoalEdit_Click(System.Object,System.Windows.RoutedEventArgs)")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:SamSmithNZ.WorldCupGoals.WPF.Teams.btnRemoveTeam_Click(System.Object,System.Windows.RoutedEventArgs)")]
