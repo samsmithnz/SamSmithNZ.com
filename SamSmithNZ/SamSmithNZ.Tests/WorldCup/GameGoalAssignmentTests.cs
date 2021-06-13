ee0e99c7cfe56a108b4122255433949e5cfc1f5c
Sam Smith (Microsoft) <samsmithnz@gmail.com>
2021-06-13 18:10:05 -0400

    Added back last two commented old historical tests

--- SamSmithNZ/SamSmithNZ.Tests/WorldCup/GameGoalAssignmentTests.cs
+++ SamSmithNZ/SamSmithNZ.Tests/WorldCup/GameGoalAssignmentTests.cs
@@ -1,54 +1,54 @@
-﻿//using System;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
-//using System.Threading.Tasks;
-//using SamSmithnNZ.Tests;
-//using SamSmithNZ.Service.Controllers.WorldCup;
-//using SamSmithNZ.Service.DataAccess.WorldCup;
-//using SamSmithNZ.Service.Models.WorldCup;
-//using System.Collections.Generic;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SamSmithnNZ.Tests;
+using SamSmithNZ.Service.DataAccess.WorldCup;
+using SamSmithNZ.Service.Models.WorldCup;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
-//namespace SamSmithNZ.Tests.WorldCup
-//{
-//    [TestClass]
-//    public class GameGoalAssignmentTests : BaseIntegrationTest
-//    {
-//        [TestMethod]
-//        public async Task GameGoalAssignmentsExistTest()
-//        {
-//            //arrange
-//            GameGoalAssignmentController controller = new GameGoalAssignmentController(new(base.Configuration));
-//            int tournamentCode = 19;
+namespace SamSmithNZ.Tests.WorldCup
+{
+    [TestClass]
+    public class GameGoalAssignmentTests : BaseIntegrationTest
+    {
+        [TestMethod]
+        public async Task GameGoalAssignmentsExistTest()
+        {
+            //arrange
+            GameGoalAssignmentDataAccess da = new(base.Configuration);
+            int tournamentCode = 19;
 
-//            //act
-//            List<GameGoalAssignment> results = await da.GetListAsync(tournamentCode);
+            //act
+            List<GameGoalAssignment> results = await da.GetList(tournamentCode);
 
-//            //assert
-//            Assert.IsTrue(results != null);
-//            Assert.IsTrue(results.Count > 0);
-//        }
+            //assert
+            Assert.IsTrue(results != null);
+            Assert.IsTrue(results.Count > 0);
+        }
 
-//        [TestMethod()]
-//        public async Task GameGoalAssignmentsFirstItemTest()
-//        {
-//            //arrange
-//            GameGoalAssignmentDataAccess da = new();
-//            int tournamentCode = 19;
+        [TestMethod()]
+        public async Task GameGoalAssignmentsFirstItemTest()
+        {
+            //arrange
+            GameGoalAssignmentDataAccess da = new(base.Configuration);
+            int tournamentCode = 19;
 
-//            //act
-//            List<GameGoalAssignment> results = await da.GetListAsync(tournamentCode);
+            //act
+            List<GameGoalAssignment> results = await da.GetList(tournamentCode);
 
-//            //assert
-//            Assert.IsTrue(results != null);
-//            Assert.IsTrue(results.Count > 0);
-//            Assert.IsTrue(results[0].GameCode > 0);
-//            Assert.IsTrue(results[0].GameNumber > 0);
-//            Assert.IsTrue(results[0].GameTime > DateTime.MinValue);
-//            Assert.IsTrue(results[0].Score != "");
-//            Assert.IsTrue(results[0].Team1Name != "");
-//            Assert.IsTrue(results[0].Team2Name != "");
-//            Assert.IsTrue(results[0].TotalGameTableGoals >= 0);
-//            Assert.IsTrue(results[0].TotalGameTablePenaltyShootoutGoals >= 0);
-//            Assert.IsTrue(results[0].TotalGoalTableGoals >= 0);
-//            Assert.IsTrue(results[0].TotalPenaltyShootoutTableGoals >= 0);        }
-//    }
-//}
+            //assert
+            Assert.IsTrue(results != null);
+            Assert.IsTrue(results.Count > 0);
+            Assert.IsTrue(results[0].GameCode > 0);
+            Assert.IsTrue(results[0].GameNumber > 0);
+            Assert.IsTrue(results[0].GameTime > DateTime.MinValue);
+            Assert.IsTrue(results[0].Score != "");
+            Assert.IsTrue(results[0].Team1Name != "");
+            Assert.IsTrue(results[0].Team2Name != "");
+            Assert.IsTrue(results[0].TotalGameTableGoals >= 0);
+            Assert.IsTrue(results[0].TotalGameTablePenaltyShootoutGoals >= 0);
+            Assert.IsTrue(results[0].TotalGoalTableGoals >= 0);
+            Assert.IsTrue(results[0].TotalPenaltyShootoutTableGoals >= 0);
+        }
+    }
+}
--- SamSmithNZ/SamSmithNZ.Tests/WorldCup/PenaltyShootoutTests.cs
+++ SamSmithNZ/SamSmithNZ.Tests/WorldCup/PenaltyShootoutTests.cs
@@ -1,89 +1,87 @@
-﻿//using System;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
-//using System.Threading.Tasks;
-//using SamSmithnNZ.Tests;
-//using SamSmithNZ.Service.Controllers.WorldCup;
-//using SamSmithNZ.Service.DataAccess.WorldCup;
-//using SamSmithNZ.Service.Models.WorldCup;
-//using System.Collections.Generic;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SamSmithnNZ.Tests;
+using SamSmithNZ.Service.DataAccess.WorldCup;
+using SamSmithNZ.Service.Models.WorldCup;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
-//namespace SamSmithNZ.Tests.WorldCup
-//{
-//    [TestClass]
-//    public class PenaltyShootoutTests : BaseIntegrationTest
-//    {
-//        [TestMethod]
-//        public async Task PenaltyShootoutsExistTest()
-//        {
-//            //arrange
-//            PenaltyShootoutGoalController controller = new PenaltyShootoutGoalController(new PenaltyShootoutGoalDataAccess(base.Configuration));
-//           int gameCode = 7376;
+namespace SamSmithNZ.Tests.WorldCup
+{
+    [TestClass]
+    public class PenaltyShootoutTests : BaseIntegrationTest
+    {
+        [TestMethod]
+        public async Task PenaltyShootoutsExistTest()
+        {
+            //arrange
+            PenaltyShootoutGoalDataAccess da = new(base.Configuration);
+            int gameCode = 7376;
 
-//            //act
-//            List<PenaltyShootoutGoal> results = await da.GetListAsync(gameCode);
+            //act
+            List<PenaltyShootoutGoal> results = await da.GetList(gameCode);
 
-//            //assert
-//            Assert.IsTrue(results != null);
-//            Assert.IsTrue(results.Count > 0);
-//        }
+            //assert
+            Assert.IsTrue(results != null);
+            Assert.IsTrue(results.Count > 0);
+        }
 
-//        [TestMethod()]
-//        public async Task PenaltyShootoutsFirstItemTest()
-//        {
-//            //arrange
-//            PenaltyShootoutGoalDataAccess da = new PenaltyShootoutGoalDataAccess();
-//            int gameCode = 7376;
+        [TestMethod()]
+        public async Task PenaltyShootoutsFirstItemTest()
+        {
+            //arrange
+            PenaltyShootoutGoalDataAccess da = new(base.Configuration);
+            int gameCode = 7376;
 
-//            //act
-//            List<PenaltyShootoutGoal> results = await da.GetListAsync(gameCode);
+            //act
+            List<PenaltyShootoutGoal> results = await da.GetList(gameCode);
 
-//            //assert
-//            Assert.IsTrue(results != null);
-//            Assert.IsTrue(results.Count > 0);
-//            Assert.IsTrue(results[0].GameCode > 0);
-//            Assert.IsTrue(results[0].PenaltyCode > 0);
-//            Assert.IsTrue(results[0].PenaltyOrder == 1);
-//            Assert.IsTrue(results[0].PlayerCode > 0);
-//            Assert.IsTrue(results[0].PlayerName == "David Luiz");
-//            Assert.IsTrue(results[0].Scored == true);
-//            Assert.IsTrue(results[1].GameCode > 0);
-//            Assert.IsTrue(results[1].PenaltyCode > 0);
-//            Assert.IsTrue(results[1].PenaltyOrder == 2);
-//            Assert.IsTrue(results[1].PlayerCode > 0);
-//            Assert.IsTrue(results[1].PlayerName == "Mauricio Pinilla");
-//            Assert.IsTrue(results[1].Scored == false);
-//        }
+            //assert
+            Assert.IsTrue(results != null);
+            Assert.IsTrue(results.Count > 0);
+            Assert.IsTrue(results[0].GameCode > 0);
+            Assert.IsTrue(results[0].PenaltyCode > 0);
+            Assert.IsTrue(results[0].PenaltyOrder == 1);
+            Assert.IsTrue(results[0].PlayerCode > 0);
+            Assert.IsTrue(results[0].PlayerName == "David Luiz");
+            Assert.IsTrue(results[0].Scored == true);
+            Assert.IsTrue(results[1].GameCode > 0);
+            Assert.IsTrue(results[1].PenaltyCode > 0);
+            Assert.IsTrue(results[1].PenaltyOrder == 2);
+            Assert.IsTrue(results[1].PlayerCode > 0);
+            Assert.IsTrue(results[1].PlayerName == "Mauricio Pinilla");
+            Assert.IsTrue(results[1].Scored == false);
+        }
 
-//        [TestMethod()]
-//        public async Task PenaltyShootoutsSaveItemTest()
-//        {
-//            //arrange
-//            PenaltyShootoutGoalDataAccess da = new PenaltyShootoutGoalDataAccess();
-//            int gameCode = 7376;
+        [TestMethod()]
+        public async Task PenaltyShootoutsSaveItemTest()
+        {
+            //arrange
+            PenaltyShootoutGoalDataAccess da = new(base.Configuration);
+            int gameCode = 7376;
 
-//            //act
-//            List<PenaltyShootoutGoal> results = await da.GetListAsync(gameCode);
-//            //Save the two goals
-//            await da.SaveItemAsync(results[0]);
-//            await da.SaveItemAsync(results[1]);
-//            //Test that the results haven't changed
-//            results = await da.GetListAsync(gameCode);
+            //act
+            List<PenaltyShootoutGoal> results = await da.GetList(gameCode);
+            //Save the two goals
+            await da.SaveItem(results[0]);
+            await da.SaveItem(results[1]);
+            //Test that the results haven't changed
+            results = await da.GetList(gameCode);
 
-//            //assert
-//            Assert.IsTrue(results != null);
-//            Assert.IsTrue(results.Count > 0);
-//            Assert.IsTrue(results[0].GameCode > 0);
-//            Assert.IsTrue(results[0].PenaltyCode > 0);
-//            Assert.IsTrue(results[0].PenaltyOrder == 1);
-//            Assert.IsTrue(results[0].PlayerCode > 0);
-//            Assert.IsTrue(results[0].PlayerName == "David Luiz");
-//            Assert.IsTrue(results[0].Scored == true);
-//            Assert.IsTrue(results[1].GameCode > 0);
-//            Assert.IsTrue(results[1].PenaltyCode > 0);
-//            Assert.IsTrue(results[1].PenaltyOrder == 2);
-//            Assert.IsTrue(results[1].PlayerCode > 0);
-//            Assert.IsTrue(results[1].PlayerName == "Mauricio Pinilla");
-//            Assert.IsTrue(results[1].Scored == false);
-//        }
-//    }
-//}
+            //assert
+            Assert.IsTrue(results != null);
+            Assert.IsTrue(results.Count > 0);
+            Assert.IsTrue(results[0].GameCode > 0);
+            Assert.IsTrue(results[0].PenaltyCode > 0);
+            Assert.IsTrue(results[0].PenaltyOrder == 1);
+            Assert.IsTrue(results[0].PlayerCode > 0);
+            Assert.IsTrue(results[0].PlayerName == "David Luiz");
+            Assert.IsTrue(results[0].Scored == true);
+            Assert.IsTrue(results[1].GameCode > 0);
+            Assert.IsTrue(results[1].PenaltyCode > 0);
+            Assert.IsTrue(results[1].PenaltyOrder == 2);
+            Assert.IsTrue(results[1].PlayerCode > 0);
+            Assert.IsTrue(results[1].PlayerName == "Mauricio Pinilla");
+            Assert.IsTrue(results[1].Scored == false);
+        }
+    }
+}
