dc3fad019f018b021830210fcc2e351ae8760f1f
Sam Smith (Microsoft) <samsmithnz@gmail.com>
2021-06-14 17:57:36 -0400

    Added new goal insights sp

---
+++ SamSmithNZ/SamSmithNZ.Database/dbo/Stored Procedures/FB_GetGoalInsights.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[FB_GetGoalInsights]
+AS
+BEGIN
+	SELECT CASE WHEN NOT injury_time IS NULL THEN CONVERT(decimal(5,1),goal_time) + 0.5 ELSE CONVERT(decimal(5,1),goal_time) END AS goal_time,
+		count(goal_code) AS goal_count
+	FROM wc_goal
+	GROUP BY CASE WHEN NOT injury_time IS NULL THEN CONVERT(decimal(5,1),goal_time) + 0.5 ELSE CONVERT(decimal(5,1),goal_time) END
+	ORDER BY CASE WHEN NOT injury_time IS NULL THEN CONVERT(decimal(5,1),goal_time) + 0.5 ELSE CONVERT(decimal(5,1),goal_time) END
+END
+GO
