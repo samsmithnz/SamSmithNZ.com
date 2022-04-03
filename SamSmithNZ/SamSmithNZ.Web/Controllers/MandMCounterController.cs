dbf93c2d7afc7fe18093a071077539e03bf47dd2
Sam Smith <samsmithnz@gmail.com>
2022-04-03 12:09:01 -0400

    Started work to allow calculations on all 3 factors

--- SamSmithNZ/SamSmithNZ.Web/Controllers/MandMCounterController.cs
+++ SamSmithNZ/SamSmithNZ.Web/Controllers/MandMCounterController.cs
@@ -15,7 +15,16 @@ namespace SamSmithNZ.Web.Controllers
             _ServiceApiClient = ServiceApiClient;
         }
 
-        public async Task<IActionResult> Index(string volumeUnit = null, float? quantity = null, float? mandMResult = null, float? peanutMandMResult = null, float? skittlesResult = null)
+        public async Task<IActionResult> Index(string volumeUnit = null,  
+            float? quantity = null, 
+            string containerUnit = null,
+            float? height = null, 
+            float? width = null, 
+            float? length = null,
+            float? radius = null,
+            float? mandMResult = null, 
+            float? peanutMandMResult = null, 
+            float? skittlesResult = null)
         {
             List<string> unitsForVolume = await _ServiceApiClient.GetUnitsForVolume();
             List<string> unitsForContainer = await _ServiceApiClient.GetUnitsForContainer();
@@ -28,6 +37,26 @@ namespace SamSmithNZ.Web.Controllers
             {
                 model.Quantity = (float)quantity;
             }
+            if (containerUnit != null)
+            {
+                model.ContainerUnit = containerUnit;
+            }
+            if (height != null)
+            {
+                model.Height = (float)height;
+            }
+            if (width != null)
+            {
+                model.Width = (float)width;
+            }
+            if (length != null)
+            {
+                model.Length = (float)length;
+            }
+            if (radius != null)
+            {
+                model.Radius = (float)radius;
+            }
             if (mandMResult != null)
             {
                 model.MandMResult = (float)mandMResult;
@@ -39,6 +68,15 @@ namespace SamSmithNZ.Web.Controllers
             if (mandMResult != null)
             {
                 model.SkittlesResult = (float)skittlesResult;
+            }
+            model.ActiveTab = "0";
+            if (radius != null)
+            {
+                model.ActiveTab = "2";
+            }
+            else if (length != null)
+            {
+                model.ActiveTab = "1";
             }
 
             return View(model);
@@ -65,7 +103,61 @@ namespace SamSmithNZ.Web.Controllers
                 peanutMandMResult = peanutMandMResult.ToString("0.0"),
                 skittlesResult = skittlesResult.ToString("0.0")
             });
+        }
 
+        public async Task<IActionResult> RunRectangleCalculation(string ContainerUnit, string txtHeight, string txtWidth, string txtLength)
+        {
+            float mandMResult = 0f;
+            float peanutMandMResult = 0f;
+            float skittlesResult = 0f;
+            float height;
+            float width;
+            float length;
+            if (float.TryParse(txtHeight, out height) == true &&
+                float.TryParse(txtWidth, out width) == true &&
+                float.TryParse(txtLength, out length) == true)
+            {
+                mandMResult = await _ServiceApiClient.GetMandMDataForRectangle(ContainerUnit, height, width, length);
+                peanutMandMResult = await _ServiceApiClient.GetPeanutMandMDataForRectangle(ContainerUnit, height, width, length);
+                skittlesResult = await _ServiceApiClient.GetSkittlesDataForRectangle(ContainerUnit, height, width, length);
+            }
+
+            return RedirectToAction("Index", new
+            {
+                containerUnit = ContainerUnit,
+                height = txtHeight,
+                width = txtWidth,
+                length = txtLength,
+                mandMResult = mandMResult.ToString("0.0"),
+                peanutMandMResult = peanutMandMResult.ToString("0.0"),
+                skittlesResult = skittlesResult.ToString("0.0")
+            });
+        }
+
+        public async Task<IActionResult> RunCylinderCalculation(string ContainerUnit, string txtHeight, string txtRadius)
+        {
+            float mandMResult = 0f;
+            float peanutMandMResult = 0f;
+            float skittlesResult = 0f;
+            float height;
+            float radius;
+            if (float.TryParse(txtHeight, out height) == true &&
+                float.TryParse(txtRadius, out radius) == true)
+            {
+                mandMResult = await _ServiceApiClient.GetMandMDataForCylinder(ContainerUnit, height, radius);
+                peanutMandMResult = await _ServiceApiClient.GetPeanutMandMDataForCylinder(ContainerUnit, height, radius);
+                skittlesResult = await _ServiceApiClient.GetSkittlesDataForCylinder(ContainerUnit, height, radius);
+            }
+
+            return RedirectToAction("Index", new
+            {
+                containerUnit = ContainerUnit,
+                height = txtHeight,
+                radius = txtRadius,
+                mandMResult = mandMResult.ToString("0.0"),
+                peanutMandMResult = peanutMandMResult.ToString("0.0"),
+                skittlesResult = skittlesResult.ToString("0.0")
+            });
         }
 
         public IActionResult About()
