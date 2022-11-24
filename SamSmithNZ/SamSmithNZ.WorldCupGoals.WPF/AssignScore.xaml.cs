7f461b219311ef20fce3be3a2f54dd0d37d78d0f
Sam Smith <samsmithnz@gmail.com>
2022-11-24 10:02:14 -0500

    Added new spain test

--- SamSmithNZ/SamSmithNZ.WorldCupGoals.WPF/AssignScore.xaml.cs
+++ SamSmithNZ/SamSmithNZ.WorldCupGoals.WPF/AssignScore.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using SamSmithNZ.Service.DataAccess.WorldCup;
+using SamSmithNZ.Service.Models.Steam;
 using SamSmithNZ.Service.Models.WorldCup;
 using System;
 using System.Diagnostics;
@@ -7,6 +8,8 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Xml.Linq;
+using static SamSmithNZ.Service.DataAccess.WorldCup.EloRating;
 
 namespace SamSmithNZ.WorldCupGoals.WPF
 {
@@ -114,6 +117,18 @@ namespace SamSmithNZ.WorldCupGoals.WPF
                     }
 
                     GameDataAccess da = new(_configuration);
+                    //Get the ELO rating updates
+                    EloRating eloRating = new();
+                    WhoWonEnum? whoWonGame = eloRating.WhoWon(_game);
+                    double kFactor = eloRating.CalculateKFactor(_game);
+                    (int, int) newEloRatings = eloRating.GetEloRatingScoresForMatchUp(_game.Team1PreGameEloRating,
+                        _game.Team2PreGameEloRating,
+                        whoWonGame == WhoWonEnum.Team1,
+                        whoWonGame == WhoWonEnum.Team2,
+                        kFactor);
+                    _game.Team1PostGameEloRating = newEloRatings.Item1;
+                    _game.Team2PostGameEloRating = newEloRatings.Item2;
+                    //Save the game
                     await da.SaveItem(_game);
 
                     lblStatus.Content = "Updating ELO ratings...";
