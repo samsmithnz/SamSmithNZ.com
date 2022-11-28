ee9a7f5937a1f06bec94020615987fda48e9b37b
Sam Smith <samsmithnz@gmail.com>
2022-11-28 17:36:59 -0500

    Added new stat

---
+++ SamSmithNZ/SamSmithNZ.Service/Models/WorldCup/StatsAverageTournamentGoals.cs
@@ -0,0 +1,11 @@
+﻿namespace SamSmithNZ.Service.Models.WorldCup
+{
+    public class StatsAverageTournamentGoals
+    {
+        public int TournamentCode { get; set; }
+        public string TournamentName { get; set; }
+        public decimal TotalGamesCompleted { get; set; }
+        public decimal TotalGoals { get; set; }
+        public decimal AverageGoalsPerGame { get; set; }
+    }
+}
