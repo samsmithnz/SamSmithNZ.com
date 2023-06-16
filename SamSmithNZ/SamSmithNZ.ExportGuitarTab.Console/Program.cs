7bf9c1cd793a8d4679b51461e111f412a955a382
Sam Smith <samsmithnz@gmail.com>
2023-06-16 13:48:43 -0400

    Adding bass albums

--- SamSmithNZ/SamSmithNZ.ExportGuitarTab.Console/Program.cs
+++ SamSmithNZ/SamSmithNZ.ExportGuitarTab.Console/Program.cs
@@ -26,68 +26,84 @@ namespace SamSmithNZ.ExportGuitarTab.Console
             //Get all artists and their albums
             //ArtistDataAccess artistDataAccess = new(configuration);
             AlbumDataAccess albumDataAccess = new(configuration);
-            TabDataAccess tabDataAccess = new(configuration);
             //List<Artist> artists = await artistDataAccess.GetList(true, true);
             List<Album> albums = await albumDataAccess.GetList(true);
 
             foreach (Album album in albums)
             {
+                if (album.ArtistName == "Foo Fighters")
+                {
+                    continue;
+                }
                 System.Console.WriteLine($"Processing album {album.AlbumName}");
-                //Get the tabs in order of track number
-                List<Tab> tabs = await tabDataAccess.GetList(album.AlbumCode, 0);
 
-                //Create the album directory if it doesn't exist
+                await ProcessAlbum(targetDirectory, album, configuration);
+                if (album.BassAlbumCode != 0)
+                {
+                    Album baseAlbum = await albumDataAccess.GetItem(album.BassAlbumCode, true);
+                    await ProcessAlbum(targetDirectory, baseAlbum, configuration);
+                }
+            }
+
+            TimeSpan timeSpan = DateTime.Now - startTime;
+            System.Console.WriteLine($"Processing completed in {timeSpan.TotalSeconds} seconds");
+        }
+
+        private async static Task ProcessAlbum(string targetDirectory,
+            Album album,
+            IConfiguration configuration)
+        {
+            //Get the tabs in order of track number
+            TabDataAccess tabDataAccess = new(configuration);
+            List<Tab> tabs = await tabDataAccess.GetList(album.AlbumCode, 0);
+
+            //strip out invalid directory characters
+            string artistName = StripOutInvalidWindowsNaming(album.ArtistName);
+            string albumName = StripOutInvalidWindowsNaming(album.AlbumName);
+            string isBassAlbum = "";
+            if (album.IsBassTab == true)
+            {
+                isBassAlbum = "_Bass";
+            }
+            string albumDirectory = targetDirectory + "\\" + artistName + "\\" + albumName + isBassAlbum;
+            //Create the album directory if it doesn't exist
+            if (Directory.Exists(albumDirectory) == false)
+            {
+                System.Console.WriteLine($"Creating new directory '{albumDirectory}'");
+                Directory.CreateDirectory(albumDirectory);
+            }
+
+            //Create a file for each tab
+            foreach (Tab tab in tabs)
+            {
+                string tabName = tab.TabName;
                 //strip out invalid file name characters
-                string artistName = StripOutInvalidWindowsNaming(album.ArtistName);
-                string albumName = StripOutInvalidWindowsNaming(album.AlbumName);
-                string isBassAlbum = "";
-                if (album.IsBassTab == true)
-                {
-                    isBassAlbum = "_Bass";
-                }
-                string albumDirectory = targetDirectory + "\\" + artistName + "\\" + albumName + isBassAlbum;
-                if (Directory.Exists(albumDirectory) == false)
-                {
-                    System.Console.WriteLine($"Creating new directory '{albumDirectory}'");
-                    Directory.CreateDirectory(albumDirectory);
-                }
+                tabName = StripOutInvalidWindowsNaming(tabName);
+                string tabFileName = albumDirectory + "\\" + tab.TabOrder.ToString("00") + "." + tabName + ".txt";
 
-                //Create a file for each tab
-                foreach (Tab tab in tabs)
+                if (File.Exists(tabFileName) == false)
                 {
-                    string tabName = tab.TabName;
-                    //strip out invalid file name characters
-                    tabName = StripOutInvalidWindowsNaming(tabName);
-                    string tabFileName = albumDirectory + "\\" + tab.TabOrder.ToString("00") + "." + tabName + ".txt";
-
-                    if (File.Exists(tabFileName) == false)
+                    System.Console.WriteLine($"Creating new file '{tabFileName}'");
+                    using (StreamWriter sw = File.CreateText(tabFileName))
                     {
-                        System.Console.WriteLine($"Creating new file '{tabFileName}'");
+                        sw.WriteLine(tab.TabText);
+                    }
+                }
+                else
+                {
+                    //Compare the existing file contents with the database contents
+                    string existingFileContents = File.ReadAllText(tabFileName);
+                    if (existingFileContents != tab.TabText + "\r\n")
+                    {
+                        System.Console.WriteLine($"Updating file '{tabFileName}'");
                         using (StreamWriter sw = File.CreateText(tabFileName))
                         {
                             sw.WriteLine(tab.TabText);
                         }
                     }
-                    else
-                    {
-                        //Compare the existing file contents with the database contents
-                        string existingFileContents = File.ReadAllText(tabFileName);
-                        if (existingFileContents != tab.TabText + "\r\n")
-                        {
-                            System.Console.WriteLine($"Updating file '{tabFileName}'");
-                            using (StreamWriter sw = File.CreateText(tabFileName))
-                            {
-                                sw.WriteLine(tab.TabText);
-                            }
-                        }
-                    }
-
                 }
 
             }
-
-            TimeSpan timeSpan = DateTime.Now - startTime;
-            System.Console.WriteLine($"Processing completed in {timeSpan.TotalSeconds} seconds");
         }
 
         private static string StripOutInvalidWindowsNaming(string name)
