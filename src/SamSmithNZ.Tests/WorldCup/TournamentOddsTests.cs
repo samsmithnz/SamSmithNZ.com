7132fa39a2bc020da5352313fb966649b03c17cc
Sam Smith <samsmithnz@gmail.com>
2024-06-17 10:38:46 -0400

    first pass of odds testing

---
+++ src/SamSmithNZ.Tests/WorldCup/TournamentOddsTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SamSmithnNZ.Tests;
+using SamSmithNZ.Service.Controllers.WorldCup;
+using SamSmithNZ.Service.DataAccess.WorldCup;
+using SamSmithNZ.Service.Models.WorldCup;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using SamSmithNZ.Service;
+using System.Diagnostics;
+
+namespace SamSmithNZ.Tests.WorldCup
+{
+    [TestClass]
+    [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+    public class TournamentOddsTests : BaseIntegrationTest
+    {
+        [TestMethod()]
+        public async Task TouramentEuro2024OddsTest()
+        {
+            //arrange
+            GameController gameController = new(new GameDataAccess(base.Configuration));
+            int tournamentCode = 317;
+
+            //act
+            List<Game> games = await gameController.GetGamesByTournament(tournamentCode);
+            foreach (Game game in games)
+            {
+                int team1Won = 0;
+                int team2Won = 0;
+                int sampleSize = 10000;
+                double team1ChanceToWin = game.Team1ChanceToWin;
+                double team2ChanceToWin = game.Team2ChanceToWin;
+                Debug.WriteLine(game.Team1Name + " has a " + team1ChanceToWin + "% chance to win against " + game.Team2Name);
+                for (int i = 0; i < sampleSize; i++)
+                {
+                    if (Utility.GenerateRandomNumber(0, 100) < (team1ChanceToWin / 100))
+                    {
+                        team1Won++;
+                    }
+                    else
+                    {
+                        team2Won++;
+                    }
+                }
+                Debug.WriteLine(game.Team1Name + " won " + team1Won + " times, for " + ((double)team1Won / (double)sampleSize).ToString("0.00%") + " of the time against " + game.Team2Name);
+                break;
+            }
+
+        }
+    }
+}
