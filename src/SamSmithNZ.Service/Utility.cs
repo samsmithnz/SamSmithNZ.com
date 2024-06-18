3b34c1c67e20d5f8ac40f20be4858786ff70763f
Sam Smith <samsmithnz@gmail.com>
2024-06-18 16:23:59 -0400

    Fix to game odds

--- src/SamSmithNZ.Service/Utility.cs
+++ src/SamSmithNZ.Service/Utility.cs
@@ -5,7 +5,7 @@
         private readonly static System.Random rnd = new();
         public static double GenerateRandomNumber(int lowerBound, int upperBound)
         {
-            double result = rnd.Next(lowerBound, upperBound + 1) / 100; // +1 because the upperbound is never used
+            double result = rnd.Next(lowerBound, upperBound + 1); // +1 because the upperbound is never used
             return result;
         }
     }
